9e8d5790c93cb54beb0605ad3d27fa6eb7c17e44
tizot <masset.camille@gmail.com>
2015-07-25 23:15:19 +0200

    Initial commit. Event module

---
+++ jtx_events/models.py
@@ -0,0 +1,41 @@
+import datetime
+
+from django.db import models
+
+from rest_framework import serializers, viewsets
+
+
+class Event(models.Model):
+    """
+    An event covered by the JTX, or during which have taken place one or more projections
+    """
+    class Meta:
+        app_label: 'jtx_event'
+    title = models.CharField(max_length=254)
+    description = models.TextField()
+    begin_date = models.DateTimeField(default=datetime.datetime.now())
+    end_date = models.DateTimeField(default=datetime.datetime.now())
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+
+    _type = VirtualField("Event")
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    filter_fields = {
+        'begin_date': ['lte', 'gte'],
+        'end_date': ['lte', 'gte'],
+    }
+    search_fields = ('title', 'description')
