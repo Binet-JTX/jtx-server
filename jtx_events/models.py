bcff6a6a467c3d61b16cf7254192f2bc7e99f3aa
tizot <masset.camille@gmail.com>
2015-07-26 13:57:31 +0200

    Handle Event cancel/restore

--- jtx_events/models.py
+++ jtx_events/models.py
@@ -2,8 +2,10 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.http import Http404
 
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, decorators
+from rest_framework.response import Response
 
 
 class Event(models.Model):
@@ -26,6 +28,12 @@ class Event(models.Model):
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
+        read_only_fields = ('deleted_at', 'deleted', )
+
+    def validate(self, data):
+        if data['begin_date'] > data['end_date']:
+            raise serializers.ValidationError("end date must occur after begin date")
+        return data
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -36,3 +44,31 @@ class EventViewSet(viewsets.ModelViewSet):
         'end_date': ['lte', 'gte'],
     }
     search_fields = ('title', 'description')
+
+    @decorators.detail_route(methods=['put'])
+    def cancel(self, request, pk=None):
+        try:
+            event = Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            raise Http404()
+
+        event.deleted = True
+        event.deleted_at = timezone.now()
+        event.save()
+
+        serializer = self.get_serializer_class()(event)
+        return Response(serializer.data)
+
+    @decorators.detail_route(methods=['put'])
+    def restore(self, request, pk=None):
+        try:
+            event = Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            raise Http404()
+
+        event.deleted = False
+        event.deleted_at = None
+        event.save()
+
+        serializer = self.get_serializer_class()(event)
+        return Response(serializer.data)
