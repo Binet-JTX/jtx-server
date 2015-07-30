152c991f732a9337499d1e382739294447bfcf4a
tizot <masset.camille@gmail.com>
2015-07-30 22:24:05 +0200

    Reading permission for anybody and admin registering

--- jtx/settings/common.py
+++ jtx/settings/common.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': (
         # 'rest_framework.permissions.IsAdminUser',
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
         # 'rest_framework.permissions.AllowAny',
 
     ),
--- jtx_core/admin.py
+++ jtx_core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from jtx_core.models.tag import Tag, TagKey
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(TagKey)
